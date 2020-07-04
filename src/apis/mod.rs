46a782064b5d692391310e3d62a80f074f5cb544
Henning Holm <git@henningholm.de>
2020-07-04 16:03:50 +0200

    Regenerate code with new generator version

--- src/apis/mod.rs
+++ src/apis/mod.rs
@@ -1,45 +1,63 @@
-use std::fmt;
+use reqwest;
+use serde_json;
+use std::{error, fmt};
+
+#[derive(Debug, Clone)]
+pub struct ResponseContent<T> {
+    pub status: reqwest::StatusCode,
+    pub content: String,
+    pub entity: Option<T>,
+}
+
+impl<T> fmt::Display for ResponseContent<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.status, self.content)
+    }
+}
 
 #[derive(Debug)]
-pub enum Error {
+pub enum Error<T> {
     Reqwest(reqwest::Error),
     Serde(serde_json::Error),
     Io(std::io::Error),
+    ResponseError(ResponseContent<T>),
 }
 
-impl fmt::Display for Error {
+impl<T> fmt::Display for Error<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Error::Reqwest(e) => fmt::Display::fmt(e, f),
             Error::Serde(e) => fmt::Display::fmt(e, f),
             Error::Io(e) => fmt::Display::fmt(e, f),
+            Error::ResponseError(e) => fmt::Display::fmt(e, f),
         }
     }
 }
 
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+impl<T: fmt::Debug> error::Error for Error<T> {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
             Error::Reqwest(err) => Some(err),
             Error::Serde(err) => Some(err),
             Error::Io(err) => Some(err),
+            Error::ResponseError(_) => None,
         }
     }
 }
 
-impl From<reqwest::Error> for Error {
+impl <T> From<reqwest::Error> for Error<T> {
     fn from(e: reqwest::Error) -> Self {
         Error::Reqwest(e)
     }
 }
 
-impl From<serde_json::Error> for Error {
+impl <T> From<serde_json::Error> for Error<T> {
     fn from(e: serde_json::Error) -> Self {
         Error::Serde(e)
     }
 }
 
-impl From<std::io::Error> for Error {
+impl <T> From<std::io::Error> for Error<T> {
     fn from(e: std::io::Error) -> Self {
         Error::Io(e)
     }
@@ -49,30 +67,18 @@ pub fn urlencode<T: AsRef<str>>(s: T) -> String {
     ::url::form_urlencoded::byte_serialize(s.as_ref().as_bytes()).collect()
 }
 
-mod actions_api;
-pub use self::actions_api::{ ActionsApi, ActionsApiClient };
-mod datacenters_api;
-pub use self::datacenters_api::{ DatacentersApi, DatacentersApiClient };
-mod floating_ips_api;
-pub use self::floating_ips_api::{ FloatingIpsApi, FloatingIpsApiClient };
-mod images_api;
-pub use self::images_api::{ ImagesApi, ImagesApiClient };
-mod isos_api;
-pub use self::isos_api::{ IsosApi, IsosApiClient };
-mod locations_api;
-pub use self::locations_api::{ LocationsApi, LocationsApiClient };
-mod networks_api;
-pub use self::networks_api::{ NetworksApi, NetworksApiClient };
-mod pricing_api;
-pub use self::pricing_api::{ PricingApi, PricingApiClient };
-mod server_types_api;
-pub use self::server_types_api::{ ServerTypesApi, ServerTypesApiClient };
-mod servers_api;
-pub use self::servers_api::{ ServersApi, ServersApiClient };
-mod ssh_keys_api;
-pub use self::ssh_keys_api::{ SshKeysApi, SshKeysApiClient };
-mod volumes_api;
-pub use self::volumes_api::{ VolumesApi, VolumesApiClient };
+pub mod actions_api;
+pub mod datacenters_api;
+pub mod floating_ips_api;
+pub mod images_api;
+pub mod isos_api;
+pub mod locations_api;
+pub mod networks_api;
+pub mod pricing_api;
+pub mod server_types_api;
+pub mod servers_api;
+pub mod ssh_keys_api;
+pub mod volumes_api;
 
-pub mod configuration;
 pub mod client;
+pub mod configuration;
