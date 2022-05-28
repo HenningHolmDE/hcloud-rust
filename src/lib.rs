a5b77b8ac8f4bd0ef08370a036e796ff44a83619
Henning Holm <git@henningholm.de>
2022-05-28 21:41:41 +0200

    Update readme

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //!
 //!# Current state of development
 //!
-//!- supported API endpoints (complete as of May 2022): `actions`, `certificates`, `datacenters`, `firewalls`, `floating_ips`, `images`, `isos`, `load_balancer_types`, `load_balancers`, `locations`, `networks`, `placement_groups`, `pricing`, `placement_groups`, `server_types`, `servers`, `ssh_keys`, `volumes`
+//!- supported API endpoints (complete as of May 2022): `actions`, `certificates`, `datacenters`, `firewalls`, `floating_ips`, `images`, `isos`, `load_balancer_types`, `load_balancers`, `locations`, `networks`, `placement_groups`, `pricing`, `server_types`, `servers`, `ssh_keys`, `volumes`
 //!- asynchronous API functions
 //!- pagination support
 //!- documentation and tests are still WIP
