d91a9230e0e2e6f5a7e95fa0b31f317a6451c2fd
Henning Holm <git@henningholm.de>
2023-05-20 11:09:24 +0200

    Bump crate version to 0.14.0

--- src/apis/configuration.rs
+++ src/apis/configuration.rs
@@ -38,7 +38,7 @@ impl Default for Configuration {
     fn default() -> Self {
         Configuration {
             base_path: "https://api.hetzner.cloud/v1".to_owned(),
-            user_agent: Some("hcloud-rust/0.13.0".to_owned()),
+            user_agent: Some("hcloud-rust/0.14.0".to_owned()),
             client: reqwest::Client::new(),
             basic_auth: None,
             oauth_access_token: None,
