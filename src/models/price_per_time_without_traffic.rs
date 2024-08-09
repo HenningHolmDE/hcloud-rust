a785d8138be856cee513e8e2e43fda436acb0075
Henning Holm <git@henningholm.de>
2024-08-09 16:23:37 +0200

    Use hcloud-openapi 0.21.0
    Changes in the new version of `hcloud-openapi`:
    - Update to Upstream Spec File
      This release includes a lot of changes, many of them breaking.
      Due to the size of the update, detailed change notes won't be
      provided.

--- src/models/price_per_time_without_traffic.rs
+++ src/models/price_per_time_without_traffic.rs
@@ -12,35 +12,26 @@ use crate::models;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
-pub struct PricePerTime {
-    /// Free traffic per month in bytes.
-    #[serde(rename = "included_traffic")]
-    pub included_traffic: i64,
-    /// Name of the Location the price is for. | Name of the Location the price is for
+pub struct PricePerTimeWithoutTraffic {
+    /// Name of the Location the price is for
     #[serde(rename = "location")]
     pub location: String,
     #[serde(rename = "price_hourly")]
     pub price_hourly: Box<models::Price>,
     #[serde(rename = "price_monthly")]
     pub price_monthly: Box<models::Price>,
-    #[serde(rename = "price_per_tb_traffic")]
-    pub price_per_tb_traffic: Box<models::Price>,
 }
 
-impl PricePerTime {
+impl PricePerTimeWithoutTraffic {
     pub fn new(
-        included_traffic: i64,
         location: String,
         price_hourly: models::Price,
         price_monthly: models::Price,
-        price_per_tb_traffic: models::Price,
-    ) -> PricePerTime {
-        PricePerTime {
-            included_traffic,
+    ) -> PricePerTimeWithoutTraffic {
+        PricePerTimeWithoutTraffic {
             location,
             price_hourly: Box::new(price_hourly),
             price_monthly: Box::new(price_monthly),
-            price_per_tb_traffic: Box::new(price_per_tb_traffic),
         }
     }
 }
--- src/models/rule_response.rs
+++ src/models/rule_response.rs
@@ -11,9 +11,8 @@
 use crate::models;
 use serde::{Deserialize, Serialize};
 
-/// Rule : Rule of a firewall.
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
-pub struct Rule {
+pub struct RuleResponse {
     /// Description of the Rule
     #[serde(
         rename = "description",
@@ -23,37 +22,37 @@ pub struct Rule {
     )]
     pub description: Option<Option<String>>,
     /// List of permitted IPv4/IPv6 addresses for outgoing traffic (`direction` set to `out`) in [CIDR block notation](https://wikipedia.org/wiki/CIDR). You can specify 100 CIDR blocks at most.  The CIDR blocks may refer to networks (with empty host bits) or single hosts. For example, a network could be defined with `10.0.1.0/24` or `2001:db8:ff00:42::/64`, and a single host with `10.0.1.1/32` or `2001:db8:ff00:42::8329/128`. Use `0.0.0.0/0` to allow any IPv4 addresses and `::/0` to allow any IPv6 addresses.
-    #[serde(rename = "destination_ips", skip_serializing_if = "Option::is_none")]
-    pub destination_ips: Option<Vec<String>>,
+    #[serde(rename = "destination_ips")]
+    pub destination_ips: Vec<String>,
     /// Select traffic direction on which rule should be applied. Use `source_ips` for direction `in` and `destination_ips` for direction `out`.
     #[serde(rename = "direction")]
     pub direction: Direction,
     /// Port or port range to which traffic will be allowed, only applicable for protocols TCP and UDP. A port range can be specified by separating two ports with a dash, e.g `1024-5000`.
-    #[serde(
-        rename = "port",
-        default,
-        with = "::serde_with::rust::double_option",
-        skip_serializing_if = "Option::is_none"
-    )]
-    pub port: Option<Option<String>>,
+    #[serde(rename = "port", deserialize_with = "Option::deserialize")]
+    pub port: Option<String>,
     /// Type of traffic to allow
     #[serde(rename = "protocol")]
     pub protocol: Protocol,
     /// List of permitted IPv4/IPv6 addresses for incoming traffic (`direction` set to `in`) in [CIDR block notation](https://wikipedia.org/wiki/CIDR). You can specify 100 CIDR blocks at most.  The CIDR blocks may refer to networks (with empty host bits) or single hosts. For example, a network could be defined with `10.0.1.0/24` or `2001:db8:ff00:42::/64`, and a single host with `10.0.1.1/32` or `2001:db8:ff00:42::8329/128`. Use `0.0.0.0/0` to allow any IPv4 addresses and `::/0` to allow any IPv6 addresses.
-    #[serde(rename = "source_ips", skip_serializing_if = "Option::is_none")]
-    pub source_ips: Option<Vec<String>>,
+    #[serde(rename = "source_ips")]
+    pub source_ips: Vec<String>,
 }
 
-impl Rule {
-    /// Rule of a firewall.
-    pub fn new(direction: Direction, protocol: Protocol) -> Rule {
-        Rule {
+impl RuleResponse {
+    pub fn new(
+        destination_ips: Vec<String>,
+        direction: Direction,
+        port: Option<String>,
+        protocol: Protocol,
+        source_ips: Vec<String>,
+    ) -> RuleResponse {
+        RuleResponse {
             description: None,
-            destination_ips: None,
+            destination_ips,
             direction,
-            port: None,
+            port,
             protocol,
-            source_ips: None,
+            source_ips,
         }
     }
 }
